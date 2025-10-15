490b5968e45df4a5ce48ee3773cb2833435a5cf9
OLDFish <87295394+123eyu@users.noreply.github.com>
2025-10-15 19:57:48 +0800

    chore: 更新导航数据 - 2025/10/15 19:57:47

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -337,10 +337,82 @@ export const mockData = {
     },
     {
       "id": "learning",
-      "name": "资源网站",
+      "name": "网站资源",
       "icon": "📚",
       "order": 6,
-      "sites": []
+      "sites": [
+        {
+          "id": "site-1760528642681",
+          "name": "国际学生证生成器",
+          "url": "https://student.frp.gs/",
+          "description": "国际学生证生成器"
+        },
+        {
+          "id": "site-1760528688176",
+          "name": "UUID生成器",
+          "url": "https://1024tools.com/uuid",
+          "description": "UUID生成器"
+        },
+        {
+          "id": "site-1760528753706",
+          "name": "ping-ip",
+          "url": "https://www.itdog.cn/",
+          "description": "检测ip全国ping和速度"
+        },
+        {
+          "id": "site-1760528777233",
+          "name": "测网速",
+          "url": "https://www.cesu.net/",
+          "description": "测试网速",
+          "icon": ""
+        },
+        {
+          "id": "site-1760528815979",
+          "name": "查看本机ip",
+          "url": "https://iplark.com/",
+          "description": "检测查看本地ip",
+          "icon": ""
+        },
+        {
+          "id": "site-1760528961553",
+          "name": "ip检测",
+          "url": "https://ipinfo.io/products/ip-company-api",
+          "description": "ip检测"
+        },
+        {
+          "id": "site-1760528983538",
+          "name": "ip检测",
+          "url": "https://demo.fastlyedge.com/",
+          "description": "ip检测",
+          "icon": ""
+        },
+        {
+          "id": "site-1760529013015",
+          "name": "测速",
+          "url": "https://www.speedtest.net/",
+          "description": "测速",
+          "icon": ""
+        },
+        {
+          "id": "site-1760529088482",
+          "name": "google测速",
+          "url": "https://fiber.google.com/speedtest/",
+          "description": "google测速"
+        },
+        {
+          "id": "site-1760529118600",
+          "name": "DNS测试泄露",
+          "url": "https://browserleaks.com/dns",
+          "description": "DNS测试泄露"
+        },
+        {
+          "id": "site-1760529157515",
+          "name": "美国地址生成器",
+          "url": "https://ratenn.com/american.html",
+          "description": "美国地址生成器",
+          "icon": ""
+        }
+      ]
     },
     {
       "id": "tools",
