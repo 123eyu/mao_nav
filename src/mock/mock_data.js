994507f2088e7a6b44e12bbe999e7942ff1c7751
OLDFish <87295394+123eyu@users.noreply.github.com>
2025-10-15 20:13:18 +0800

    chore: 更新导航数据 - 2025/10/15 20:13:18

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -408,6 +408,18 @@ export const mockData = {
           "url": "https://duckyci.com/auth/sign-in?redirect=https%3A%2F%2Fnext.duckyci.com%2F%3Fregion%3Djp-tokyo%26availability_domain%3D1%26fault_domain%3Dauto%26shape%3DVM.Optimized.D1.Mini%26ipv4%3D1%26ipv6_subnet%3D1%26image%3Ddebian11%26capacity_type%3Don-demand%26pay_cycle%3Dmonthly",
           "description": "全是NAT小鸡很便宜，只是速度很垃圾",
           "icon": ""
+        },
+        {
+          "id": "site-1760530345814",
+          "name": "ipraft，双isp",
+          "url": "https://ipraft.com/",
+          "description": "双isp，价格3刀"
+        },
+        {
+          "id": "site-1760530396060",
+          "name": "日本家宽",
+          "url": "https://lxc.miraigrid.com/",
+          "description": "日本家宽35/月"
         }
       ]
     },
